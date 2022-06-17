97f83f91fd7daff6adb047df5c3e39410ed258bc
Phillip Bercot <phillip.bercot@nucor.com>
2022-06-17 16:30:21 -0400

    added an express web app for front end

---
+++ index.ts
@@ -0,0 +1,7 @@
+let app = document.getElementById("app");
+let htmlCanvas = document.getElementById("kami-canvas");
+
+let p = document.createElement("p");
+p.textContent = "KAMI";
+
+app?.appendChild(p);
